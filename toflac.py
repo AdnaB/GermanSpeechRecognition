6f5129cc34d084e3aa02e618e78839bd1eaa88ad
Sanne <sanneeggengoor@gmail.com>
2019-07-03 11:56:57 +0200

    oei oei

--- toflac.py
+++ toflac.py
@@ -21,7 +21,7 @@ files = [f for f in listdir(import_folder) if isfile(join(import_folder, f))]
 #       counter += 1
 
 
-for file in files
+for file in files:
     # file = file[:-3]
     song = AudioSegment.from_mp3(import_folder + "/" +file)
     song.export(export_folder+"/"+file[:-4]+".wav",format = "wav")
