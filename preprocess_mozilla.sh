a093dabb64078328d01e60fc1fe9779242522e67
Sanne <sanneeggengoor@gmail.com>
2019-07-03 22:14:03 +0200

    ai ai

--- preprocess_mozilla.sh
+++ preprocess_mozilla.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
 #SBATCH --mem=32GB
 module load Python/3.6.4-foss-2018a
 python3 /data/s3757994/GermanSpeechRecognition/util/preprocess_mozilla.py
