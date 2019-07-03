24122ee13a1340d0cc3f6257db16d8e1cfbc56c6
Sanne <sanneeggengoor@gmail.com>
2019-07-03 11:54:10 +0200

    getting there hopelijk

--- preprocess_mozilla.sh
+++ preprocess_mozilla.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32GB
-
+module load Python/3.6.4-foss-2018a
 module load FFmpeg
-/home/s3757994/GermanSpeechRecognition/toflac.py
-/home/s3757994/GermanSpeechRecognition/util/preprocess_mozilla.py
+/data/s3757994/GermanSpeechRecognition/toflac.py
+/data/s3757994/GermanSpeechRecognition/util/preprocess_mozilla.py
