b453c719642ade2f3beb955d8515ec39b92fdffc
Sanne <sanneeggengoor@gmail.com>
2019-07-03 11:56:19 +0200

    sorry voor de spam

--- preprocess_mozilla.sh
+++ preprocess_mozilla.sh
@@ -5,5 +5,5 @@
 #SBATCH --mem=32GB
 module load Python/3.6.4-foss-2018a
 module load FFmpeg
-/data/s3757994/GermanSpeechRecognition/toflac.py
-/data/s3757994/GermanSpeechRecognition/util/preprocess_mozilla.py
+python3 /data/s3757994/GermanSpeechRecognition/toflac.py
+python3 /data/s3757994/GermanSpeechRecognition/util/preprocess_mozilla.py
