d12b4a45aff3ee103429a1678a376f8686471ea6
Sanne <sanneeggengoor@gmail.com>
2019-07-03 22:02:12 +0200

    nog een pgoing

--- preprocess_mozilla.sh
+++ preprocess_mozilla.sh
@@ -4,5 +4,4 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=32GB
 module load Python/3.6.4-foss-2018a
-python3 /data/s3757994/GermanSpeechRecognition/util/extreemsimpel.py
-# python3 /data/s3757994/GermanSpeechRecognition/util/preprocess_mozilla.py
+python3 /data/s3757994/GermanSpeechRecognition/util/preprocess_mozilla.py
