cb5f595fae1e1593ee9097f0033424ac93eff8ed
Sanne <sanneeggengoor@gmail.com>
2019-07-03 21:34:43 +0200

    please

--- preprocess_mozilla.sh
+++ preprocess_mozilla.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=10
+#SBATCH --ntasks=1
 #SBATCH --mem=32GB
 module load Python/3.6.4-foss-2018a
 module load FFmpeg
-# python3 /data/s3757994/GermanSpeechRecognition/toflac.py
 python3 /data/s3757994/GermanSpeechRecognition/util/preprocess_mozilla.py
